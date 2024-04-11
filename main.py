a4655c36f767353c3cf564c1d4c8bb0b5a443cef
dongshin <leened@naver.com>
2024-04-11 12:02:11 +0000

    init: main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+#initial version
+
+from typing import List
+
+def twoSum(nums: List[int], target: int) -> List[int]:
+    n = len(nums)
+
+    for i in range(n):
+        for j in range(i+1, n):
+            if nums[i] + nums[j] == target:
+                return [i,j]
+    
+    return []
+
